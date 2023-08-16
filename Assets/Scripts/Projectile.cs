1b32832d76707995ab867aed6b0c7458cfae24d1
Md Kamruzzaman Tusher <8888749+tkamruzzaman@users.noreply.github.com>
2023-08-16 17:45:19 +0600

    starter_project

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 10;
+    public Rigidbody2D rb2d;
+    public float deathDelay = 5;
+
+    public bool disabled = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d.velocity = transform.up * speed;
+        StartCoroutine(DeathAfterDelay(deathDelay));
+    }
+
+    private IEnumerator DeathAfterDelay(float deathDelay)
+    {
+        yield return new WaitForSeconds(deathDelay);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<Projectile>() || collision.GetComponent<Enemy>() || collision.GetComponent<Player>())
+            Destroy(gameObject);
+    }
+}
